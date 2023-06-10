67bda69124caff673856b3d27c17bc6b90dd7134
Ermek22-01 <ermektoktosunov4@gmail.com>
2023-06-10 11:18:14 +0300

    commit homework8

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+function Tabs({ children }) {
+  const [activeTab, setActiveTab] = React.useState(0);
 
-function App() {
+  const handleTabClick = (index) => {
+    setActiveTab(index);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <div className="tabs">
+          {React.Children.map(children, (child, index) => {
+            return React.cloneElement(child, {
+              active: index === activeTab,
+              onClick: () => handleTabClick(index),
+            });
+          })}
+        </div>
+        <div className="content">
+          {React.Children.toArray(children)[activeTab]}
+        </div>
+      </div>
   );
 }
 
+function Tab({ active, onClick, children }) {
+  return (
+      <div className={active ? 'tab active' : 'tab'} onClick={onClick}>
+        {children}
+      </div>
+  );
+}
+
+function App() {
+  return (
+      <Tabs>
+        <Tab>Вкладка 1</Tab>
+        <Tab>Вкладка 2</Tab>
+        <Tab>Вкладка 3</Tab>
+      </Tabs>
+  );
+}
 export default App;
